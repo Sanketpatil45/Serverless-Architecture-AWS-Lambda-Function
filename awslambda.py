914780e3fe488c9fc091bc20a682164a778e56d0
Sanket Patil <55378662+Sanketpatil45@users.noreply.github.com>
2023-09-09 13:20:09 +0530

    Update awslambda.py

--- awslambda.py
+++ awslambda.py
@@ -5,6 +5,7 @@ ec2_connection=boto3. resource(service_name="ec2", region_name="ap-south-1")
 filter_ec2tag={"Name": "tag: Env", "Values":["Test"]}
 for each in ec2_connection.instances.filter(Filters=[filter_ec2tag]):
 each.stop()
+#each.start()
 
 return {
 'statusCode': 200,
